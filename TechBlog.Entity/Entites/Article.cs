4b06271dab74bd868a08779aa7cd2dcd289b653f
Kanan Huseynov <info.kennans26@gmail.com>
2024-02-24 20:45:14 +0400

    Created some Entity (EL classes) and EntityBase class as input.

---
+++ TechBlog.Entity/Entites/Article.cs
@@ -0,0 +1,16 @@
+using TechBlog.Core.Entities;
+
+namespace TechBlog.Entity.Entites;
+
+public class Article : EntityBase
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int ViewCount { get; set; }
+
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public Guid ImageId { get; set; }
+    public Image Image { get; set; }
+}
---
+++ TechBlog.Entity/Entites/Image.cs
@@ -0,0 +1,10 @@
+using TechBlog.Core.Entities;
+
+namespace TechBlog.Entity.Entites;
+
+public class Image : EntityBase
+{
+    public string FileName { get; set; }
+    public string FileType { get; set; }
+    public ICollection<Article> Articles { get; set; }
+}
